14977aa0610ebb9da9c584f3ba0ee8ffbe976e4d
Roman Beauvallet <roman.beauvallet@ens-lyon.fr>
2025-11-06 21:05:36 +0100

    HUGE cleaning and structuring

--- bnn_package/evolution.py
+++ bnn_package/evolution.py
@@ -4,6 +4,7 @@
 
 import numpy as np
 import numba
+from scipy.spatial.distance import pdist
 
 # ======================= Functions
 
@@ -71,58 +72,3 @@ def evolution_vec(X_0, Y_0, N, list_ab, Eps, Adjacence):
         # This slice (a row) is contiguous by default!
         X_c, Y_c = transfo_coupling_vec(X[t, :], Y[t, :], Eps, Adjacence)
     return X, Y
-
-
-@numba.jit(nopython=True)
-def MSD_xy(G, X, Y):
-    """
-    Return the MSD for a given epsilon
-    """
-    n, N = X.shape
-    assert n == len(G), "wrong dimension"
-    MSD_values = np.zeros(N)
-    mean_X = np.mean(X, axis=0)
-    mean_Y = np.mean(Y, axis=0)
-    for t in range(N):
-        MSD_t = np.mean((X[:, t] - mean_X[t]) ** 2 + (Y[:, t] - mean_Y[t]) ** 2)
-        MSD_values[t] = MSD_t
-    return MSD_values
-
-
-def MSD_vec_xy(G, X, Y):
-    """
-    Return the MSD for a given epsilon
-    """
-    n, N = X.shape
-    assert n == len(G), "wrong dimension"
-    MSD_values = np.zeros(N)
-    mean_X = np.mean(X, axis=0)
-    mean_Y = np.mean(Y, axis=0)
-    MSD_values = np.mean(
-        (X - mean_X[np.newaxis, :]) ** 2 + (Y - mean_Y[np.newaxis, :]) ** 2, axis=1
-    )
-    return MSD_values
-
-
-def MSD(G, X, axe=1):
-    """
-    Return the MSD in the order:
-    -- axe=1: esperance_t(variance_node(t))
-    -- axe=0: esperance_node(variance_t(node))
-    """
-    n, N = X.shape
-    assert n == len(G), "wrong dimension"
-    MSD_values = np.std(X, axis=axe)
-    return np.mean(MSD_values)
-
-
-def MSD_inverse(G, X, axe=0):
-    """
-    Return the MSD in the order:
-    -- axe=1: esperance_t(variance_node(t))
-    -- axe=0: esperance_node(variance_t(node))
-    """
-    n, N = X.shape
-    assert n == len(G), "wrong dimension"
-    MSD_values = np.mean(X, axis=axe)
-    return np.std(MSD_values)
