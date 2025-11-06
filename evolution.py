682c0b7c2de911760d8c7eb3b3c2e51809b3efc1
Roman Beauvallet <roman.beauvallet@ens-lyon.fr>
2025-11-06 11:16:21 +0100

    Clear output for two node + improvement evolution function for contiguity slicing

--- evolution.py
+++ evolution.py
@@ -50,17 +50,62 @@ def transfo_coupling_vec(X, Y, Eps, Adjacence):
 
 @numba.jit(nopython=True)
 def evolution_vec(X_0, Y_0, N, list_ab, Eps, Adjacence):
+    """
+    Warning: slicing on rows gives contiguous array while on columns it is not.
+
+    Return the evolution with respect to time of the nodes.
+    """
     n, m = len(X_0), len(Y_0)
     p = len(Adjacence)
     assert n == m, "wrong dimensions, both initial conditions don't have the same size"
-    X = np.zeros((n, N))
-    Y = np.zeros((n, N))
+    assert p == n, "missmatch dimensions"
+    X = np.zeros((N, n)) 
+    Y = np.zeros((N, n))
     UN = np.ones(p)
-    X[:, 0] = X_0
-    Y[:, 0] = Y_0
-    X_c, Y_c = transfo_coupling_vec(X[:, 0], Y[:, 0], Eps, Adjacence)
+    X[0, :] = X_0 
+    Y[0, :] = Y_0
+    X_c, Y_c = transfo_coupling_vec(X[0, :], Y[0, :], Eps, Adjacence)
     for t in range(1, N):
-        X[:, t] = -list_ab[0, :] * X_c**2 + UN + Y_c
-        Y[:, t] = list_ab[1, :] * X_c
-        X_c, Y_c = transfo_coupling_vec(X[:, t], Y[:, t], Eps, Adjacence)
+        X[t, :] = -list_ab[0, :] * X_c**2 + UN + Y_c 
+        Y[t, :] = list_ab[1, :] * X_c
+        # This slice (a row) is contiguous by default!
+        X_c, Y_c = transfo_coupling_vec(X[t, :], Y[t, :], Eps, Adjacence)
     return X, Y
+
+
+@numba.jit(nopython=True)
+def MSD_xy(G, X, Y):
+    """
+    Return the MSD for a given epsilon
+    """
+    n, N = X.shape
+    assert n == len(G), "wrong dimension"
+    MSD_values = np.zeros(N)
+    mean_X = np.mean(X, axis=0)
+    mean_Y = np.mean(Y, axis=0)
+    for t in range(N):
+        MSD_t = np.mean((X[:, t] - mean_X[t]) ** 2 + (Y[:, t] - mean_Y[t]) ** 2)
+        MSD_values[t] = MSD_t
+    return MSD_values
+
+
+def MSD_vec_xy(G, X, Y):
+    """
+    Return the MSD for a given epsilon
+    """
+    n, N = X.shape
+    assert n == len(G), "wrong dimension"
+    MSD_values = np.zeros(N)
+    mean_X = np.mean(X, axis=0)
+    mean_Y = np.mean(Y, axis=0)
+    MSD_values = np.mean(
+        (X - mean_X[np.newaxis, :]) ** 2 + (Y - mean_Y[np.newaxis, :]) ** 2, axis=1
+    )
+    return MSD_values
+
+
+def MSD_vec(G, X):
+    n, N = X.shape
+    assert n == len(G), "wrong dimension"
+    MSD_values = np.std(X, axis=0)
+    return np.mean(MSD_values)
